b4635fbee4f6c0e77a54a6cbf9fb7490f6791be7
Yuki Ueno <y0uk10126@gmail.com>
2023-03-05 23:12:15 +0900

    アプリケーションのビルド

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,21 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true,
+
+//   pluginOptions: {
+//     vuetify: {
+// 			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+// 		}
+//   }
+// })
+
+module.exports = {
+  outputDir: 'docs',
+  publicPath: './',
 
   pluginOptions: {
     vuetify: {
 			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
 		}
   }
-})
+}
